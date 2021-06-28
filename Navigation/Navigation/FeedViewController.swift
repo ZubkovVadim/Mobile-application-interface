89c9fa8dd002e3037bf3142be337cf9b10fc1f9f
ZubkovVadim <zubkov.vadimnn@mail.ru>
2021-06-28 14:42:32 +0300

    task-2_update Changed button on FeedViewController Deleted comments Changed button on InfoView

--- Navigation/Navigation/FeedViewController.swift
+++ Navigation/Navigation/FeedViewController.swift
@@ -14,7 +14,6 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let vc = segue.destination
